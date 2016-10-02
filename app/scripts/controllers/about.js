77b0754c49b23c34202f5e354d9a4dd2f9490f71
namilar <n.radith@gmail.com>
2016-10-02 10:31:28 +0530

    initialize project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the saApp
+ */
+angular.module('saApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name saApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the saApp
+ */
+angular.module('saApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
